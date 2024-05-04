c41d1bc388fdd01cec33d935c9eb63ff81d5dd69
Juan Benavides Avila <juanbenavidesavila@gmail.com>
2024-05-04 18:25:22 -0400

    p7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -23,8 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
@@ -34,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.core:core-ktx:+")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/alkewalletandroid/MainActivity7.java
+++ app/src/main/java/com/example/alkewalletandroid/MainActivity7.java
@@ -1,6 +1,10 @@
 package com.example.alkewalletandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,24 @@ public class MainActivity7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+
+        ImageView volcerCuentaUsuario = findViewById(R.id.volver_cuenta);
+        TextView volverCuentaUsuario2 = findViewById(R.id.volver_cuenta2);
+
+        volcerCuentaUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity7.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+
+        volverCuentaUsuario2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity7.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
