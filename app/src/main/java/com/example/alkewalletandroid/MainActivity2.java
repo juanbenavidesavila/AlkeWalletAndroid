8ee6cc4fcf2e2bc97912751b9e1af89bbf96732d
Juan Benavides Avila <juanbenavidesavila@gmail.com>
2024-05-04 17:16:28 -0400

    p1

--- app/src/main/java/com/example/alkewalletandroid/MainActivity2.java
+++ app/src/main/java/com/example/alkewalletandroid/MainActivity2.java
@@ -14,6 +14,5 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
     }
 }
