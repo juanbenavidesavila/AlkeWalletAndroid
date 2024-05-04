b28d16930009f80825d922ff0151237e8ed30cc1
Juan Benavides Avila <juanbenavidesavila@gmail.com>
2024-05-04 17:16:28 -0400

    p2

--- app/src/main/java/com/example/alkewalletandroid/MainActivity.java
+++ app/src/main/java/com/example/alkewalletandroid/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.alkewalletandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView next= findViewById(R.id.logo);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/alkewalletandroid/MainActivity2.java
+++ app/src/main/java/com/example/alkewalletandroid/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.alkewalletandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,23 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        Button registro = findViewById(R.id.entrar);
+        TextView ccuenta = findViewById(R.id.c_cuenta);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        ccuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
